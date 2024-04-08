1d8032e8fdb2541fa8954fb385a3251f195ce3e7
chen <81369809+Rainblooding@users.noreply.github.com>
2024-04-08 09:30:40 +0800

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,15 +1,3 @@
-# Install Python
-curl https://www.python.org/ftp/python/3.9.2/Python-3.9.2.tgz -o Python-3.9.2.tgz
-tar -xzvf Python-3.9.2.tgz
-cd Python-3.9.2
-./configure --enable-optimizations
-make -j $(nproc)
-make install
-
-# Install pip
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-
 pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
