c8a6fe2dfb9d71320b79932d7d57b33918f1cd82
chen <81369809+Rainblooding@users.noreply.github.com>
2024-04-08 09:02:58 +0800

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,3 +1,12 @@
+
+# Install Python
+apt-get update
+apt-get install -y python3
+
+# Install pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+
 pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
